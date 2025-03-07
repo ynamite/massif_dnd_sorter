961548634493c7912fcd67e0aa50886a939d4f78
ynamite <ynamite@bluewin.ch>
2025-03-07 13:17:32 +0100

    fix: only show if table is set

--- boot.php
+++ boot.php
@@ -6,15 +6,16 @@ use Ynamite\DndSorter;
 
 if (rex::isBackend() && rex::getUser()) {
 
+    rex_view::addCssFile($this->getAssetsUrl('style.css'));
+
     $table_name = rex_request('table_name', 'string', '');
 
     if (!$table_name) return;
 
     $DndSorter = new DndSorter($table_name);
-    if (!$DndSorter) return;
+    if (!$DndSorter->getValue('table_name')) return;
 
     $DndSorter->addExtensions();
 
-    rex_view::addCssFile($this->getAssetsUrl('style.css'));
     rex_view::addJsFile($this->getAssetsUrl('scripts.js'));
 }
